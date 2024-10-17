4c317e157c0dc52882f25d8a4eb11a18fd668a42
plvo <plvdev@proton.me>
2024-10-17 15:59:00 +0000

    add(simpleasynctask)

---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.simpleasynctask;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mTextView = (TextView) findViewById(R.id.textView1);
+
+        if(savedInstanceState!=null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask(View view) {
+        mTextView.setText(R.string.napping);
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
